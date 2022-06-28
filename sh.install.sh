a7f33db35b27ee294fc91feb7f8bbf9bbf69b649
Ajay o s <akhubsmaster@gmail.com>
2022-06-28 19:44:51 +0530

    Update sh.install.sh

--- sh.install.sh
+++ sh.install.sh
@@ -2,3 +2,7 @@
 
 cd $PROFILE
 git clone https://github.com/B-SUDO-003/codespace.git.sh  .blacksudo
+echo "removing .bashrc"
+# rm .bashrc
+echo "adding new .bashrc"
+cp .blacksudo/.bashrc bash.bashrc
